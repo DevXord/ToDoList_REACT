890cba1eb86cf3cbedfdbdb871fa8b74665743df
DevXord <xordii.xd@gmail.com>
2024-08-28 20:46:49 +0200

    router.js

--- src/util/router.js
+++ src/util/router.js
@@ -10,10 +10,13 @@ import TasksPage from "../pages/TasksPage";
 import RootLayouts from "../layouts/RootLayouts";
   
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route element={<RootLayouts />}>
-      <Route path="/" element={<HomePage />} />
+  createRoutesFromElements(<Route element={<RootLayouts />}> 
+      <Route index path="/" element={<HomePage />} />
       <Route path="/Tasks" element={<TasksPage />} />
     </Route> 
-  )
+  ),
+  {
+    basename: process.env.PUBLIC_URL,
+  }
 ); 
 export {router}
